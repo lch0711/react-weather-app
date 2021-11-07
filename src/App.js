3f5504867c26c929749e567e89f6612716f13062
lch0711 <lchung0711@gmail.com>
2021-11-07 16:36:46 +0000

    Installed Bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Weather App</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
