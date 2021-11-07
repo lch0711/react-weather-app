6abf67358c2730759b53d7a9e6a380b11614e6c6
lch0711 <lchung0711@gmail.com>
2021-11-07 17:58:46 +0000

    Added html and css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Weather App</h1>
+        <Weather />
+        <footer>
+          Coded by Lai Shuan Chung and is{" "}
+          <a
+            href="https://github.com/lch0711/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
       </div>
     </div>
   );
---
+++ src/Weather.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Search for a City..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input type="submit" value="Go" className="btn btn-danger" />
+          </div>
+        </div>
+      </form>
+      <h1>Southampton, UK</h1>
+      <ul>
+        <li>Sunday 17:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Weather-Icon"
+          ></img>
+          6°C
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Humidity: 72%</li>
+            <li>Wind: 13km/h</li>
+            <li>Cloudiness: 20%</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
