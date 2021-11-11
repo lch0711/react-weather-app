3512accd45f4c8a71c607d79c2d0da8226af1786
lch0711 <lchung0711@gmail.com>
2021-11-11 17:08:26 +0000

    Updated time display

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -14,13 +14,13 @@ export default function FormattedDate(props) {
   let hours = props.date.getHours();
 
   if (hours < 10) {
-    hours = `${0}hours`;
+    hours = `0${hours}`;
   }
 
   let minutes = props.date.getMinutes();
 
   if (minutes < 10) {
-    minutes = `${0}minutes`;
+    minutes = `0${minutes}`;
   }
 
   return (
